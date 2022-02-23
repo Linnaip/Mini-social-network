4ef8d57fb64f5af61f082b1ec8ae0c399cb6157c
Linnaip <96427250+Linnaip@users.noreply.github.com>
2022-02-23 17:57:46 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def index(request):
+    """Главная страница."""
+    return HttpResponse('Главная страница')
+
+
+def group_posts(request, slug):
+    """Страница с инфорацией о Вайфу."""
+    return HttpResponse(f'Лучшая вайфу:{slug}')

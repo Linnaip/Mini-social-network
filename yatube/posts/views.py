cd771dc6b26ed2d7c736722bea72b985c3657c42
Linnaip <linnaip@yandex.ru>
2022-03-16 15:11:36 +0300

    Изменен index с подгрузкой из базы данных

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
+
+
 # Create your views here.
 
 
 def index(request):
     """Главная страница."""
-    return HttpResponse('Главная страница')
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
-    """Страница с инфорацией о Вайфу."""
-    return HttpResponse(f'Лучшая вайфу:{slug}')
+    """Страница с инфорацией о Post."""
+    template = 'posts/group_posts.html'
+    title = 'Лев Толстой – зеркало русской революции.'
+    context = {
+        'title': title
+    }
+    return HttpResponse(request, slug, template, context)

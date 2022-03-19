2efff04be2fa2fc5d66abf03ed8ba8d40f83497e
Lychagin Andrew <linnaip@yandex.ru>
2022-03-19 13:37:39 +0300

    Убраны лишнии комментарии

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
 
-# Create your views here.
-
-
 def index(request):
     """Главная страница."""
     posts = Post.objects.order_by('-pub_date')[:10]

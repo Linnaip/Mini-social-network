8b3c973534d279044587aa09238a5be2ac6a38d2
Lychagin Andrew <linnaip@yandex.ru>
2022-03-22 00:07:33 +0300

    Исправлены ошибки

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-# Create your models here.
+
 User = get_user_model()
 
 
@@ -30,4 +30,4 @@ class Post(models.Model):
     )
 
     class Meta:
-        ordering = ['pub_date']
+        ordering = ['-pub_date']

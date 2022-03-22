3bb78c2d0fc003ac736764715f35fbcf514b8d2e
Lychagin Andrew <linnaip@yandex.ru>
2022-03-22 11:50:17 +0300

    Добавлена константа

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -15,7 +15,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+# CONSTANT
+CONSTANT = 10
 # Application definition
 
 INSTALLED_APPS = [
@@ -59,7 +60,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'yatube.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -69,7 +69,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -89,7 +88,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -102,7 +100,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/

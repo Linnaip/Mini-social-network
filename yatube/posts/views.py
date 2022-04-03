887be59b7ab6e4ae537fe2c7528df3b095d71247
Lychagin Andrew <linnaip@yandex.ru>
2022-04-03 18:46:16 +0300

    Авторизация

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.conf import settings as s
 from django.shortcuts import render, get_object_or_404
 
@@ -6,9 +7,12 @@ from .models import Group, Post
 
 def index(request):
     """Главная страница."""
-    posts = Post.objects.all()[:s.CONSTANT]
+    posts_list = Post.objects.all()[:s.CONSTANT]
+    paginator = Paginator(posts_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, 'posts/index.html', context)
 

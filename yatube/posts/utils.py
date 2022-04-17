3b99c33009e63de12bf52427a0857683f972709e
Lychagin Andrew <linnaip@yandex.ru>
2022-04-17 14:48:40 +0300

    Коментарии к функциям

--- yatube/posts/utils.py
+++ yatube/posts/utils.py
@@ -1,8 +1,8 @@
-from django.core.paginator import Paginator
 from django.conf import settings as s
+from django.core.paginator import Paginator
 
 
-# Код работы с пагинатором
 def get_page_context(posts_list):
+    """Код работы пагинатора."""
     paginator = Paginator(posts_list, s.CONSTANT)
     return paginator
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,4 @@
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator
-from django.conf import settings as s
 from django.shortcuts import render, redirect, get_object_or_404
 
 from .forms import PostForm

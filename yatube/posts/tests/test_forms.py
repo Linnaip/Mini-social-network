e5643197167da09f9055a00b4b907fbf62153d37
Andrey Mac <linnaip@yandex.ru>
2022-05-04 13:31:47 +0300

    Написаны тесты для post_create и post_edit

---
+++ yatube/posts/tests/test_forms.py
@@ -0,0 +1,58 @@
+from http import HTTPStatus
+
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from django.urls import reverse
+
+from ..forms import PostForm
+from ..models import Group, Post
+
+User = get_user_model()
+
+
+class PostViewTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create_user(username='auth')
+        cls.group = Group.objects.create(
+            title='Тестовая группа',
+            slug='test-slug',
+            description='Тестовое описание',
+        )
+        cls.post = Post.objects.create(
+            author=cls.user,
+            text='Тестовый текст поста', )
+        cls.form = PostForm()
+
+    def setUp(self):
+        self.author = User.objects.create_user(username='HasNoName')
+        self.authorized_client = Client()
+        self.authorized_client.force_login(self.user)
+
+    def test_create_post_page(self):
+        posts_count = Post.objects.count()
+        form_data = {
+            'text': 'Тестовый текс',
+            'group': self.group.id
+        }
+        response = self.authorized_client.post(
+            reverse('posts:post_create'),
+            data=form_data,
+        )
+        self.assertEqual(Post.objects.count(), posts_count + 1)
+        self.assertRedirects(response, reverse('posts:profile',
+                                               kwargs={'username': f'{self.user}'}))
+
+    def test_authorized_edit_post(self):
+        form_data = {
+            'text': 'Измененный текст'
+        }
+        response = self.authorized_client.post(
+            reverse('posts:post_edit', kwargs={'post_id': f'{self.post.pk}'}),
+            data=form_data,
+            follow=True,
+        )
+        post_2 = Post.objects.get(id=self.group.pk)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(post_2.text, 'Измененный текст')

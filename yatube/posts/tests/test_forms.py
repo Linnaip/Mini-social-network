8414ab7c31fcd8b1c205ebc481302e90936acb57
Andrey Mac <linnaip@yandex.ru>
2022-05-11 22:36:27 +0300

    Написан тест для картинки в формах

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -1,14 +1,20 @@
+import shutil
+import tempfile
 from http import HTTPStatus
 
+from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.test import Client, TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import Client, TestCase, override_settings
 from django.urls import reverse
 
 from ..models import Group, Post
 
 User = get_user_model()
+TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
 
 
+@override_settings(MEDIA_ROOT=TEMP_MEDIA_ROOT)
 class PostViewTest(TestCase):
     @classmethod
     def setUpClass(cls):
@@ -22,6 +28,11 @@ class PostViewTest(TestCase):
         cls.post = Post.objects.create(
             author=cls.user,
             text='Тестовый текст поста', )
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        shutil.rmtree(TEMP_MEDIA_ROOT, ignore_errors=True)
 
     def setUp(self):
         self.author = User.objects.create_user(username='HasNoName')
@@ -54,3 +65,32 @@ class PostViewTest(TestCase):
         post_2 = Post.objects.get(id=self.post.pk)
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(post_2.text, 'Измененный текст')
+
+    def test_create_post_with_image_page(self):
+        """Валидная форма создает запись с картинкой."""
+        posts_count = Post.objects.count()
+        small_gif = (
+            b'\x47\x49\x46\x38\x39\x61\x02\x00'
+            b'\x01\x00\x80\x00\x00\x00\x00\x00'
+            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+            b'\x0A\x00\x3B'
+        )
+        uploaded = SimpleUploadedFile(
+            name='small.gif',
+            content=small_gif,
+            content_type='image/gif'
+        )
+        form_data = {
+            'title': 'Тестовый заголовок',
+            'text': 'Тестовый текст',
+            'image': uploaded,
+        }
+        response = self.authorized_client.post(
+            reverse('posts:post_create'),
+            data=form_data,
+        )
+        self.assertRedirects(response, reverse('posts:profile',
+                                               kwargs={'username': self.user}))
+        self.assertEqual(Post.objects.count(), posts_count + 1)

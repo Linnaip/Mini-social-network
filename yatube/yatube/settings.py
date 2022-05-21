53aec37623c416e43a866c6e681bb803366bb997
Andrey Mac <linnaip@yandex.ru>
2022-05-21 14:53:58 +0300

    Caches

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -20,6 +20,12 @@ ALLOWED_HOSTS = [
     'testserver',
 ]
 
+#Кэширование
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
 # EmailBackend
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')

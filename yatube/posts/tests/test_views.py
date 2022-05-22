81e3bce104ccee2a25efb30c8040328bbb9e2d67
Andrey Mac <linnaip@yandex.ru>
2022-05-22 20:30:30 +0300

    import

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -8,7 +8,7 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import Client, TestCase, override_settings
 from django.urls import reverse
 
-from ..models import Post, Group, Comment
+from ..models import Post, Group, Comment, Follow
 
 User = get_user_model()
 TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
